8a9ce7fb47231c39a85b77e32e9b21f1ea4043d1
Supphawit Hemhiranrasm <115527684+SupphawitH@users.noreply.github.com>
2025-09-16 16:37:56 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+def main():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    result  = num1*num2
+    print(f"{num1} x {num2} = {result}")
+    if result > 0:
+        print("The result is positive")
+    elif result < 0:
+        print("The result is negative")
+    else:
+        print("The result is positive and negative")
+        
+main()
